84eed6ac12916a015c14904fae9d898dcdabc67a
varsha-0411 <boppanavarsha411@gmail.com>
2026-03-08 17:57:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
-const PORT = 80;
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
